7ae06fe5017e4edaeb317ccf401f1882338aac8b
Linni <linnea@ossiansson.se>
2025-04-28 10:19:24 +0200

    inspector window added

--- test/quick_test.py
+++ test/quick_test.py
@@ -4,11 +4,11 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 import dearpygui.dearpygui as dpg
-from editor.interface_manager import InterfaceManager
+from editor.display_manager import DisplayManager
 from editor.window.main_window import MainWindow
 
 # Create an instance of InterfaceManager   
-manager = InterfaceManager()
+manager = DisplayManager()
 # Create a MainWindow instance
 main_window = MainWindow("Main Window")
 
